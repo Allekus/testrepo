011d00b2d2a2d23c20eed855f2e30aebc0a49c77
Allekus <166712798+Allekus@users.noreply.github.com>
2024-07-13 18:50:36 +0200

    Add files via upload
    Fichero subido al repositorio

---
+++ test.py
@@ -0,0 +1,8 @@
+def mi_funcion(nombre, apellido):
+    print('saludos desde mi función')
+    print(f'Nombre: {nombre}, Apellido: {apellido}')
+    # Sería como imprimir así:
+    print('Nombre:', nombre, 'Apellido:', apellido)
+
+mi_funcion('Juan', 'Perez')
+mi_funcion('Karla','Lara')
